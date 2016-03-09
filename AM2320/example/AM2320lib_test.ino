251820c4305b5ecb0e31dd70311c79cd54ec26a1
Ten04031977 <ten04031977@mail.ru>
2016-03-09 22:18:44 +0300

    Update AM2320lib_test.ino

--- AM2320/example/AM2320lib_test.ino
+++ AM2320/example/AM2320lib_test.ino
@@ -1,19 +1,27 @@
 #include <Wire.h>
 #include <AM2320.h>
 AM2320 th;
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  
-  Serial.println("---------------");
-  Serial.print("humidity: ");
-  Serial.print(th.getHumidity());
-  Serial.print("%");
-  Serial.print("|| temperature: ");
-  Serial.print(th.getTemperature());
-  Serial.println("*C");
-  Serial.println("---------------");
-  delay(2000);
+  switch(th.Read()) {
+    case 2:
+      Serial.println("CRC failed");
+      break;
+    case 1:
+      Serial.println("Sensor offline");
+      break;
+    case 0:
+      Serial.print("humidity: ");
+      Serial.print(th.h);
+      Serial.print("%, temperature: ");
+      Serial.print(th.t);
+      Serial.println("*C");
+      break;
+  }
+
+  delay(200);
 }
