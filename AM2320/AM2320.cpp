12fe12cc2636e60e3ebc6fa4d7afc0d230e0ed7a
thakshak <thakshak@users.noreply.github.com>
2019-02-19 19:43:06 -0800

    Update AM2320.cpp
    translated comments to english

--- AM2320/AM2320.cpp
+++ AM2320/AM2320.cpp
@@ -1,59 +1,55 @@
 #include "AM2320.h"
-#include <Wire.h>
-// 
-// AM2321 Temperature & Humidity Sensor library for Arduino
-// Сделана Тимофеевым Е.Н. из AM2320-master
+#include < Wire.h >
 
-unsigned int CRC16(byte *ptr, byte length) 
-{ 
-      unsigned int crc = 0xFFFF; 
-      uint8_t s = 0x00; 
+// AM2320 Temperature & Humidity Sensor library for Arduino
+// Modified by Timofeev E.N. from https://github.com/thakshak/AM2320/
 
-      while(length--) {
-        crc ^= *ptr++; 
-        for(s = 0; s < 8; s++) {
-          if((crc & 0x01) != 0) {
-            crc >>= 1; 
-            crc ^= 0xA001; 
-          } else crc >>= 1; 
-        } 
-      } 
-      return crc; 
-} 
+unsigned int CRC16(byte * ptr, byte length) {
+        unsigned int crc = 0xFFFF;
+        uint8_t s = 0x00;
 
-AM2320::AM2320()
-{
+        while (length--) {
+                crc ^ = * ptr++;
+                for (s = 0; s < 8; s++) {
+                        if ((crc & 0x01) ! = 0) {
+                                crc >> = 1;
+                                crc ^ = 0xA001;
+                        } else crc >> = 1;
+                }
+        }
+        return crc;
 }
 
-int AM2320::Read()
-{
-	byte buf[8];
-	for(int s = 0; s < 8; s++) buf[s] = 0x00; 
+AM2320::AM2320() {}
 
-	Wire.beginTransmission(AM2320_address);
-	Wire.endTransmission();
-	// запрос 4 байт (температуры и влажности)
-	Wire.beginTransmission(AM2320_address);
-	Wire.write(0x03);// запрос
-	Wire.write(0x00); // с 0-го адреса
-	Wire.write(0x04); // 4 байта
-	if (Wire.endTransmission(1) != 0) return 1;
-	delayMicroseconds(1600); //>1.5ms
-	// считываем результаты запроса
-	Wire.requestFrom(AM2320_address, 0x08); 
-	for (int i = 0; i < 0x08; i++) buf[i] = Wire.read();
+int AM2320::Read() {
+        byte buf[8];
+        for (int s = 0; s < 8; s++) buf[s] = 0x00;
 
-	// CRC check
-	unsigned int Rcrc = buf[7] << 8;
-	Rcrc += buf[6];
-	if (Rcrc == CRC16(buf, 6)) {
-		unsigned int temperature = ((buf[4] & 0x7F) << 8) + buf[5];
-		t = temperature / 10.0;
-		t = ((buf[4] & 0x80) >> 7) == 1 ? t * (-1) : t;
+        Wire.beginTransmission(AM2320_address);
+        Wire.endTransmission();
+        // request 4 bytes (temperature and humidity)
+        Wire.beginTransmission(AM2320_address);
+        Wire.write(0x03); // request
+        Wire.write(0x00); // from the 0th address
+        Wire.write(0x04); // 4 bytes
+        if (Wire.endTransmission(1) ! = 0) return 1;
+        delayMicroseconds(1600); //>1.5ms
+        // read the query results
+        Wire.requestFrom(AM2320_address, 0x08);
+        for (int i = 0; i < 0x08; i++) buf[i] = Wire.read();
 
-		unsigned int humidity = (buf[2] << 8) + buf[3];
-		h = humidity / 10.0;
-		return 0;
-	}
+        // CRC check
+        unsigned int Rcrc = buf[7] << 8;
+        Rcrc + = buf[6];
+        if (Rcrc == CRC16(buf, 6)) {
+                unsigned int temperature = ((buf[4] & 0x7F) << 8) + buf[5];
+                t = temperature / 10.0;
+                t = ((buf[4] & 0x80) >> 7) == 1 ? t * (-1) : t;
+
+                unsigned int humidity = (buf[2] << 8) + buf[3];
+                h = humidity / 10.0;
+                return 0;
+        }
         return 2;
 }
